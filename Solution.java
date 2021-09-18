bf09ddf3acd003f87ee46aebb62823ad8c601df8
Que Le <71231118+qphuong3@users.noreply.github.com>
2021-09-18 12:24:41 -0400

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int x = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] != i+1) {
+                int t = arr[i];
+                int j = 0;
+                for(; j < arr.length; j++) {
+                    if(arr[j] == i+1) {
+                        break;
+                    }
+                }
+                arr[i]=arr[j];
+                arr[j]=t;
+                x++;
+            }
+        }
+        return x;
+    }
